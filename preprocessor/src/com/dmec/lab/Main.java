47941e2250e21fd1936168a3e799ab6e8645acf1
brandonstanley <veli5470@mylaurier.ca>
2017-04-04 00:20:31 -0400

    removed test code from preprocessor

--- preprocessor/src/com/dmec/lab/Main.java
+++ preprocessor/src/com/dmec/lab/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello brandon");
+//		System.out.println("Hello brandon");
 		// TODO Auto-generated method stub
 
 	}
