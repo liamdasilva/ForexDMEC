88f8ce01c3e02ef546949f928260264cb41312e0
hasankarbalai <karb1690@mylaurier.ca>
2017-04-04 19:24:36 -0400

    Updated file uploader prototype.
    Will now write input files to WEB-INF/input folder

--- DMWeb/src/com/dmec/lab/FileUploadServlet.java
+++ DMWeb/src/com/dmec/lab/FileUploadServlet.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -22,67 +23,70 @@ import org.jboss.logging.Logger.Level;
 @WebServlet("/upload")
 @MultipartConfig
 public class FileUploadServlet extends HttpServlet {
-    private final static Logger LOGGER =
-            Logger.getLogger(FileUploadServlet.class.getCanonicalName());
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+	private final static Logger LOGGER =
+			Logger.getLogger(FileUploadServlet.class.getCanonicalName());
+	@SuppressWarnings("deprecation")
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
 
-        // Create path components to save the file
-        final String path = request.getParameter("destination");
-        final Part filePart = request.getPart("file");
-        String fileName = getFileName(filePart);
-        String[] filenameList=fileName.split("\\\\");
-        fileName = filenameList[filenameList.length-1];
-        OutputStream out = null;
-        InputStream filecontent = null;
-        final PrintWriter writer = response.getWriter();
+		// Create path components to save the file
+		//final String path = request.getParameter("destination");
+		final String path =  this.getServletContext().getRealPath("/WEB-INF/");
+		final Part filePart = request.getPart("file");
+		String fileName = getFileName(filePart);
+		String[] filenameList=fileName.split("\\\\");
+		fileName = filenameList[filenameList.length-1];
+		OutputStream out = null;
+		InputStream filecontent = null;
+		final PrintWriter writer = response.getWriter();
 
-        try {
-            out = new FileOutputStream(new File(path + File.separator
-                    + fileName));
-            filecontent = filePart.getInputStream();
 
-            int read = 0;
-            final byte[] bytes = new byte[1024];
+		try {
+			out = new FileOutputStream(new File(path +File.separator+ "input"+File.separator
+					+ fileName));
+			filecontent = filePart.getInputStream();
 
-            while ((read = filecontent.read(bytes)) != -1) {
-                out.write(bytes, 0, read);
-            }
-            writer.println("New file " + fileName + " created at " + path);
-            LOGGER.log(Level.INFO, "File{0}being uploaded to {1}",
-                    new Object[]{fileName, path});
-        } catch (FileNotFoundException fne) {
-            writer.println("You either did not specify a file to upload or are "
-                    + "trying to upload a file to a protected or nonexistent "
-                    + "location.");
-            writer.println("<br/> ERROR: " + fne.getMessage());
+			int read = 0;
+			final byte[] bytes = new byte[1024];
 
-            LOGGER.log(Level.FATAL, "Problems during file upload. Error: {0}",
-                    new Object[]{fne.getMessage()});
-        } finally {
-            if (out != null) {
-                out.close();
-            }
-            if (filecontent != null) {
-                filecontent.close();
-            }
-            if (writer != null) {
-                writer.close();
-            }
-        }
-    }
+			while ((read = filecontent.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			writer.println("New file " + fileName + " created at " + path+File.separator+"input");
+			LOGGER.log(Level.INFO, "File {0} being uploaded to {1} ",
+					new Object[]{fileName, path+File.separator+"input"});
+		} catch (FileNotFoundException fne) {
+			writer.println("You either did not specify a file to upload or are "
+					+ "trying to upload a file to a protected or nonexistent "
+					+ "location.");
+			writer.println("<br/> ERROR: " + fne.getMessage());
 
-    private String getFileName(final Part part) {
-        final String partHeader = part.getHeader("content-disposition");
-        LOGGER.log(Level.INFO, "Part Header = {0}".format( partHeader));
-        for (String content : part.getHeader("content-disposition").split(";")) {
-            if (content.trim().startsWith("filename")) {
-                return content.substring(
-                        content.indexOf('=') + 1).trim().replace("\"", "");
-            }
-        }
-        return null;
-    }	
+			LOGGER.log(Level.FATAL, "Problems during file upload. Error: {0}",
+					new Object[]{fne.getMessage()});
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+			if (filecontent != null) {
+				filecontent.close();
+			}
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+
+	private String getFileName(final Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		LOGGER.log(Level.INFO, "Part Header = {0}".format( partHeader));
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(
+						content.indexOf('=') + 1).trim().replace("\"", "");
+			}
+		}
+		return null;
+	}	
 }
