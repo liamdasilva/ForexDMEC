7c316947b4820099f334384787c71c1e0314b0d1
brandonstanley <veli5470@mylaurier.ca>
2017-04-04 04:54:48 -0400

    MA, trend, & classification (+/- num pips or ranging) calculations added

--- preprocessor/src/com/dmec/lab/Main.java
+++ preprocessor/src/com/dmec/lab/Main.java
@@ -13,55 +13,55 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-//		System.out.println("Hello brandon");
+		String filepathstr="/src/EURUSD60.csv";
+		ArrayList<Integer> movingAverages=new ArrayList<Integer>();
+		movingAverages.add(10);
+		movingAverages.add(20);
+		ArrayList<Integer> trendPeriods=new ArrayList<Integer>();
+		trendPeriods.add(3);
+		trendPeriods.add(20);
+		int pips=10;
+		
+		Preprocessor (filepathstr,movingAverages, trendPeriods, pips);
 		// TODO Auto-generated method stub
 
 	}
-	public static void preprocessor(int[] movingAverages, int[]trendPeriods, int pips, String filename){
-		/*		fw = new FileWriter("src/output.csv");
-		bw = new BufferedWriter(fw);*/
 
-	}
+	public static String calcTrend (int num, ArrayList<ArrayList<String>>dataset, int columnNum){
+		boolean trendUp = false;
+		boolean trendDown = false;
+		String RANGING="RANGING";
+		String TRENDUP="TRENDUP";
+		String TRENDDOWN="TRENDDOWN";
 
-	public static void trendPeriod(int num, int readFromColumnNum,int writeToColumnNum, int maxMovingAverage){
-
-	}
-
-	public String calcTrend (int num, ArrayList<ArrayList<String>>dataset, double range, int columnNum){
-		boolean trendUp = true;
-		boolean trendDown = true;
-		boolean flag = false;
-		if (Double.parseDouble(dataset.get(0).get(columnNum)) > Double.parseDouble(dataset.get(1).get(columnNum))){
-			trendDown = true;
-			trendUp = false;
-		}
-		else if (Double.parseDouble(dataset.get(0).get(columnNum)) < Double.parseDouble(dataset.get(1).get(columnNum))){
-			trendDown = false;
+		
+		if (Double.parseDouble(dataset.get(dataset.size()-1).get(columnNum)) > Double.parseDouble(dataset.get(dataset.size()-2).get(columnNum))){
 			trendUp = true;
 		}
-		else{
-			return "ranging";
+		else if (Double.parseDouble(dataset.get(dataset.size()-1).get(columnNum)) < Double.parseDouble(dataset.get(dataset.size()-2).get(columnNum))){
+			trendDown = true;
 		}
-		int counter = 0;
-		while(counter <dataset.size()-1){
-			if (trendDown && Double.parseDouble(dataset.get(counter).get(columnNum)) < Double.parseDouble(dataset.get(counter+1).get(columnNum))){
-				return "ranging";
+		else{
+			return RANGING;
+		}
+		int counter = 1;
+		while(counter<num-1){
+			if (trendDown && Double.parseDouble(dataset.get(dataset.size()-1-counter).get(columnNum)) > Double.parseDouble(dataset.get(dataset.size()-2-counter).get(columnNum))){
+				return RANGING;
 			}
-			if (trendUp && Double.parseDouble(dataset.get(counter).get(columnNum)) > Double.parseDouble(dataset.get(counter+1).get(columnNum))){
-				return "ranging";
+			if (trendUp && Double.parseDouble(dataset.get(dataset.size()-1-counter).get(columnNum)) < Double.parseDouble(dataset.get(dataset.size()-2-counter).get(columnNum))){
+				return RANGING;
 			}
 			counter++;
 		}
-		if (trendUp) return "trendup";
-		if (trendDown) return "trenddown";
-		return "ranging";
+		if (trendUp) return TRENDUP;
+		if (trendDown) return TRENDDOWN;
+		return RANGING;
 		
 	}
-	public static void movingAverage(int num, int readFromColumnNum,int writeToColumnNum, int maxMovingAverage){
-
-
-	}
-	public static double calcNextMovingAverage(double prevMovingAverage, int num,ArrayList<String>row, int columnNum, int prevNum){
+	
+	
+	public static double calcNextMovingAverage(double prevMovingAverage, int num,ArrayList<String>row, int columnNum, double prevNum){
 		double currentSum = prevMovingAverage*num;
 		currentSum = currentSum - prevNum + Double.parseDouble(row.get(columnNum));
 		return currentSum / num;
@@ -70,13 +70,13 @@ public class Main {
 	public static double calcInitMovingAverage (int num, ArrayList<ArrayList<String>> rows, int columnNum){
 		double sum = 0;
 		for (int i = 0; i< num;i++){
-			sum +=Double.parseDouble(rows.get(i).get(columnNum));
+			sum +=Double.parseDouble(rows.get(rows.size()-1-i).get(columnNum));
 		}
 		return sum/num;
 	}
 
 
-	public static void Preprocessor (String filepathstr,int[] movingAverages, int[]trendPeriods, int pips){
+	public static void Preprocessor (String filepathstr,ArrayList<Integer> movingAverages, ArrayList<Integer>trendPeriods, int pips){
 		String csvFile = "src/EURUSD60.csv";
 		BufferedReader br = null;
 		String line = "";
@@ -84,10 +84,24 @@ public class Main {
 		FileWriter fw = null;
 		BufferedWriter bw=null;
 
-		int maxMovingAverage = 10;
-		int num = maxMovingAverage;
-		int columnNum =5;
 		ArrayList<ArrayList<String>>dataset = new ArrayList();
+
+		int NUM_ROWS_TO_STORE=0;
+		int columnNum =5;
+		
+		//determine how many rows should be store for trend and moving average calculations
+		for(Integer movingAverageVal: movingAverages){
+			if(movingAverageVal>NUM_ROWS_TO_STORE){
+				NUM_ROWS_TO_STORE=movingAverageVal;
+			}
+		}
+		
+		for(Integer trendPeriodVal: trendPeriods){
+			if(trendPeriodVal>NUM_ROWS_TO_STORE){
+				NUM_ROWS_TO_STORE=trendPeriodVal;
+			}
+		}
+		
 
 		try {
 
@@ -95,19 +109,74 @@ public class Main {
 			fw = new FileWriter("src/output.csv");
 			bw = new BufferedWriter(fw);
 			int rowCounter =0;
-			while ((line = br.readLine()) != null & rowCounter<num) {
-				System.out.println(line);
+			while (rowCounter<NUM_ROWS_TO_STORE && (line = br.readLine()) != null) {
+//				System.out.println(line);
 				String[] row = line.split(cvsSplitBy);
 				dataset.add(new ArrayList<String>(Arrays.asList(row)));
 				rowCounter++;
 			}
-			double InitMovingAvg = calcInitMovingAverage(num, dataset, columnNum);
-			/*boolean calcTrend = 
-
-					bw.write(line+','+InitMovingAvg+'\n');
-*/			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+//			System.out.println(dataset);
+//			System.out.println(dataset.size());
+			
+			ArrayList<Double> prevMovingAverageValues=new ArrayList<Double>();
+			//calculate initial moving averages
+			for(Integer movingAverageVal: movingAverages){
+				double initMovingAvg = calcInitMovingAverage(movingAverageVal, dataset, columnNum);
+				line+=","+initMovingAvg;
+				prevMovingAverageValues.add(initMovingAvg);
+			}
+			
+			//calculate initial trends
+			for(Integer trendPeriodVal: trendPeriods){
+				String trend=calcTrend(trendPeriodVal,dataset,columnNum);
+				line+=","+trend;
+//				System.out.println(trend);
+			}
+			
+			//calculate next row trend
+			
+//			System.out.println(line);
+			bw.write(line);
+					
+			while ((line = br.readLine()) != null) {
+								
 				String[] row = line.split(cvsSplitBy);
+				dataset.add(new ArrayList<String>(Arrays.asList(row)));
+				for(int i=0;i<movingAverages.size();i++){
+					Double valueToRemove=Double.parseDouble(dataset.get(dataset.size()-1-movingAverages.get(i)).get(columnNum));
+					double movingAvg = calcNextMovingAverage(prevMovingAverageValues.get(i), movingAverages.get(i), new ArrayList<String>(Arrays.asList(row)), columnNum,valueToRemove);
+					line+=","+movingAvg;
+					prevMovingAverageValues.set(i, movingAvg);
+				}
+				
+				//calculate remaining trends
+				for(Integer trendPeriodVal: trendPeriods){
+					String trend=calcTrend(trendPeriodVal,dataset,columnNum);
+					line+=","+trend;
+				}
+				
+				//remove the first element in the dataset
+				dataset.remove(0);
+				Double prevRowClose=Double.parseDouble((dataset.get(dataset.size()-2).get(columnNum)));
+				Double currentRowClose=Double.parseDouble((dataset.get(dataset.size()-1).get(columnNum)));
+//				System.out.println(currentRowClose);
+//				System.out.println(prevRowClose);
+//				System.out.println(currentRowClose-prevRowClose>0.001);
+//				System.out.println(pips/10000.0);
+
+				String prevRowClassification;
+				if((currentRowClose-prevRowClose)>(pips/10000.0)){
+					prevRowClassification=","+"+"+pips+"pips";
+				}else if((currentRowClose-prevRowClose)<(-1*pips/10000.0)){
+					prevRowClassification=","+"-"+pips+"pips";
+
+				}else{
+					prevRowClassification=",RANGING";
+				}
+				bw.write(prevRowClassification+'\n');
+				bw.write(line);
+				
+//				rowCounter++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
