ddf986bf88adf1f463c7171c8816da161a15d5af
hasankarbalai <karb1690@mylaurier.ca>
2017-04-05 00:11:04 -0400

    Added customization fields to index.html

--- DMWeb/src/com/dmec/lab/FileUploadServlet.java
+++ DMWeb/src/com/dmec/lab/FileUploadServlet.java
@@ -71,6 +71,8 @@ public class FileUploadServlet extends HttpServlet {
 				writer.close();
 			}
 		}
+		doPreProcessing (filename)
+		response.
 	}
 
 	private String getFileName(final Part part) {
